b6fe96cea0efdd3f5a48295041b97e981bc09823
Виктория Самсонова <white013rose@gmail.com>
2023-05-02 16:27:14 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WordCounter.CountWords(true, true);
+            Console.ReadKey();
+        }        
+    }
+}
---
+++ WordCounter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Words
+{
+    public static class WordCounter
+    {   
+        /// <summary>
+        /// Count the number of words in all files and output to json file/files
+        /// </summary>
+        /// <param name="flagOtherFiles">Flag to split output by files.
+        /// true - separating output on files</param>
+        /// <param name="flagForPreposition">Flag for preposition.
+        /// true - doesn't count prepositions as words</param>
+        public static void CountWords(bool flagOtherFiles, bool flagForPreposition)
+        {
+            Console.WriteLine("Программа для подсчета слов во всех файлах и вывов в json файл.");
+            Console.WriteLine("Выполняется подсчет слов...");
+            DirectoryInfo directoryInfo = new DirectoryInfo($"{AppDomain.CurrentDomain.BaseDirectory}\\Words");
+            
+            
+            Dictionary<string, int> wordsCount = flagForPreposition ? 
+                CountWithoutPreposition() : CountWithPreposition();
+
+            if (flagOtherFiles)
+            {
+                foreach (var file in directoryInfo.GetFiles())
+                {                    
+                    string fileContent = File.ReadAllText(file.FullName);
+                    WriteWordsInFile(fileContent);
+                }
+            }
+            else
+            {
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.WriteLine($"Подсчет слов завершен. Файл находится по адресу: {AppDomain.CurrentDomain.BaseDirectory}");
+        }
+
+        private static Dictionary<string, int> CountWithPreposition()
+        {
+
+            return new Dictionary<string, int>();
+        }
+        
+        private static Dictionary<string, int> CountWithoutPreposition()
+        {
+
+            return new Dictionary<string, int>();
+        }
+
+        private static void WriteWordsInFile(string fileContent)
+        {
+            Console.WriteLine(fileContent);
+        }       
+
+    }
+}
